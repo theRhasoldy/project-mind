c062b0b4bf5706880001d008ed69d584db275f89
Nour El Din Shady <rhasoldy@gmail.com>
2023-10-16 23:36:53 +0300

    chore(project): setup essential tools

---
+++ prettier.config.mjs
@@ -0,0 +1,13 @@
+export default {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  semi: true,
+  singleQuote: false,
+  trailingComma: "es5",
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+};
