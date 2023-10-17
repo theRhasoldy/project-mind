e1f736360d7ff3c9e394445869187baa34effc2f
Nour El Din Shady <rhasoldy@gmail.com>
2023-10-17 23:40:32 +0300

    feat(radix): added global theme props

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import { Theme } from "@radix-ui/themes";
-import "./globals.css";
+import "@/styles/globals.css";
 import "@radix-ui/themes/styles.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -13,7 +13,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Theme>{children}</Theme>
+        <Theme
+          accentColor="plum"
+          grayColor="auto"
+          radius="small"
+          panelBackground="translucent"
+        >
+          {children}
+        </Theme>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <main className="bg-bronze-5 flex min-h-screen flex-col items-center justify-between p-24">
-      <Button color="plum">Mama</Button>
+    <main>
+      <Button>Mama</Button>
     </main>
   );
 }
