1f4a6b500802a73e12bbf40485792a76d5d6c32c
Nour El Din Shady <rhasoldy@gmail.com>
2023-10-20 11:22:33 +0300

    feat(tailwind): created a color palette for the app

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,36 +11,36 @@ export default {
     colors: {
       light: {
         background: {
-          default: "#3dccc7",
+          default: "#fefefe",
         },
         primary: {
-          main: "#3dccc7",
-          light: "#9ceaef",
-          dark: "#07beb8",
+          main: "#f08080",
+          light: "#f4978e",
+          dark: "#e56b6f",
           contrastText: "#fefefe",
         },
         secondary: {
-          main: "#ff0",
-          light: "#b75566",
-          dark: "#b75566",
+          main: "#7b2cbf",
+          light: "#9d4edd",
+          dark: "#5a189a",
           contrastText: "#fff",
         },
       },
       dark: {
         background: {
-          default: "#b75566",
+          default: "#0f0f0f",
         },
         primary: {
-          main: "#b75566",
-          light: "#9ceaef",
-          dark: "#07beb8",
-          contrastText: "#fefefe",
+          main: "#f8ad9d",
+          light: "#fbc4ab",
+          dark: "#e56b6f",
+          contrastText: "#000",
         },
         secondary: {
-          main: "#ff0",
-          light: "#b75566",
-          dark: "#b75566",
-          contrastText: "#fff",
+          main: "#c77dff",
+          light: "#e0aaff",
+          dark: "#9d4edd",
+          contrastText: "#000",
         },
       },
     },
